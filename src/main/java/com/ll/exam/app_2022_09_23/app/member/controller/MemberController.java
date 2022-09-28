7eb24f5a2587a3d17530327b563c8288c40d5608
NeoGuRi95 <jea5158@gmail.com>
2022-09-28 14:50:06 +0900

    rq 기능 활용

--- src/main/java/com/ll/exam/app_2022_09_23/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app_2022_09_23/app/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app_2022_09_23.app.member.controller;
 
+import com.ll.exam.app_2022_09_23.app.base.rq.Rq;
 import com.ll.exam.app_2022_09_23.app.member.dto.Member;
 import com.ll.exam.app_2022_09_23.app.member.service.MemberService;
 import com.ll.exam.app_2022_09_23.util.Util;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -14,6 +16,13 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final Rq rq;
+
+    @GetMapping("/member/me")
+    @ResponseBody
+    public Member getMe() {
+        return rq.getLoginedMember();
+    }
 
     @GetMapping("/member/login")
     public String showLogin() {
@@ -21,7 +30,7 @@ public class MemberController {
     }
 
     @PostMapping("/member/login")
-    public String login(String username, String password, HttpSession session) {
+    public String login(String username, String password) {
         Member member = memberService.getMemberByUsername(username);
 
         if (member == null) {
@@ -32,14 +41,15 @@ public class MemberController {
             return "redirect:/?msg=" + Util.url.encode("패스워드가 일치하지 않습니다.");
         }
 
-        session.setAttribute("loginedMemberId", member.getId());
+        rq.setLoginDone(member);
 
         return "redirect:/?msg=" + Util.url.encode("로그인 성공");
     }
 
     @GetMapping("/member/logout")
     public String logout(HttpSession session) {
-        session.removeAttribute("loginedMemberId");
+
+        rq.setLogoutDone();
 
         return "redirect:/?msg=" + Util.url.encode("로그아웃 성공");
     }
